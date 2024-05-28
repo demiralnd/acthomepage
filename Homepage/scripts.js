a9df16854a246c908e0abe314cd4af4fb22d84c9
demiralnd <98850888+demiralnd@users.noreply.github.com>
2024-05-28 13:15:35 +0300

    Add files via upload

---
+++ Homepage/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript loaded');
+});
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
